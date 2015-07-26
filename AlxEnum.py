3156e95337e87aae97997e24895fa713c5e677b5
Alexander Van Daele <Alex@Alexanders-MacBook-Pro.local>
2015-07-26 18:57:52 +0200

    Fleshed out setup script, starting development on enum scanner.

---
+++ AlxEnum.py
@@ -0,0 +1,17 @@
+'''
+Created on Jun 18, 2015
+
+@author: Alexander Van Daele
+'''
+
+from optparse import OptionParser
+import subprocess
+import multiprocessing
+from multiprocessing import Process, Queue
+import os
+import time 
+
+
+if __name__ == '__main__':
+    
+    pass
---
+++ Setup.py
@@ -0,0 +1,18 @@
+'''
+Created on Jun 18, 2015
+
+@author: Alexander Van Daele
+'''
+
+import os
+from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+if __name__ == '__main__':
+    setup(name='AlxEnum', version='1.0', description='Simple collection of enum scripts.', 
+          author='Alexander Van Daele',author_email='xalspacka@gmail.com', url='https://github.com/Alspacka/AlxEnum', 
+          scripts=['AlxEnum','LinuxPrivEsc','WindowsPrivEsc'],long_description=read('README.md'), 
+          packages=['Common', 'PrivilegeEscLinux','PrivilegeEscWindows','ScanModules','Scanner'],
+          data_files=[('/usr/share/AlxEnum', ['LICENSE','AlxEnumConfig.xml']), ('/usr/share/man/man1', ['Manual/AlxEnum.1.gz'])])
